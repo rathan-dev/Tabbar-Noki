2fa3de072c3d1d33c9002b0606de1c36bd30b045
rathan-dev <rathan.dhondi@thinkhat.ai>
2024-07-24 20:57:07 +0530

    Initial Commit

---
+++ Tabbar/TabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarApp.swift
+//  Tabbar
+//
+//  Created by rathan on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
